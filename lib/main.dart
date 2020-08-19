976290e7e580a080c4f54f2d3b17a433f67fdc0c
gbolahanprojects <gbolahanoduyemi1@gmail.com>
2020-08-19 21:26:25 +0100

    #feat: Added home Screen with bottomNav

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:launmax_app/homeScreen/homeScreen.dart';
 
 void main() {
   runApp(MyApp());
@@ -9,12 +10,14 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Flutter Demo',
       theme: ThemeData(
         primarySwatch: Colors.blue,
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      home: HomeScreen()
+      //MyHomePage(title: 'Flutter Demo Home Page'),
     );
   }
 }
