3f4b72a3b23b052b42e04e4b497eb11759957ab1
Seyi <seyipaye@gmail.com>
2020-08-21 01:42:16 +0100

    feat: Integration of splash screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,20 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:launmax_app/homeScreen/homeScreen.dart';
-import 'package:launmax_app/homeScreen/styles.dart';
+import 'package:launmax_app/models/app_state.dart';
+import 'package:launmax_app/styles.dart';
+import 'package:launmax_app/utils/screen_controller.dart';
+import 'package:provider/provider.dart';
 
 void main() {
-  runApp(App());
+  runApp(MyApp());
 }
 
-class App extends StatefulWidget {
-  @override
-  _AppState createState() => _AppState();
+class MyApp extends StatefulWidget {
+  _MyAppState createState() => _MyAppState();
 }
 
-class _AppState extends State<App> {
-  final firebaseApp = Firebase.initializeApp();
-
+class _MyAppState extends State<MyApp> {
   @override
   void initState() {
     super.initState();
@@ -22,67 +22,22 @@ class _AppState extends State<App> {
 
   @override
   Widget build(BuildContext context) {
-    return FutureBuilder(
-      // Initialize FlutterFire
-      future: firebaseApp,
-      builder: (context, snapshot) {
-        print(snapshot.hasError);
-        // Check for errors
-        if (snapshot.hasError) {
-          Scaffold.of(context).showSnackBar(
-            SnackBar(
-              content: Text("Sending Message"),
-            ),
-          );
-        }
-
-        // Once complete, show your application
-        if (snapshot.connectionState == ConnectionState.done) {
-          return MyApp();
-        }
-
-        // Otherwise, show something whilst waiting for initialization to complete
-        return CircularProgressIndicator();
-      },
-    );
-  }
-}
-
-class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-        debugShowCheckedModeBanner: false,
-        title: 'LaunMax',
-        theme: ThemeData(
-            primarySwatch: AppColor.primaryColor,
-            visualDensity: VisualDensity.adaptivePlatformDensity,
-            scaffoldBackgroundColor: AppColor.background),
-        home: HomeScreen()
-        //MyHomePage(title: 'Flutter Demo Home Page'),
-        );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-
-  final String title;
-
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-        backgroundColor: Colors.transparent,
-      ),
-      body: Center(child: Container()),
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider(
+          create: (context) => AppState(),
+        ),
+      ],
+      child: MaterialApp(
+          debugShowCheckedModeBanner: false,
+          title: 'LaunMax',
+          theme: ThemeData(
+              primarySwatch: AppColor.primaryColor,
+              visualDensity: VisualDensity.adaptivePlatformDensity,
+              scaffoldBackgroundColor: AppColor.background),
+          home: ScreenController()
+          //MyHomePage(title: 'Flutter Demo Home Page'),
+          ),
     );
   }
 }
