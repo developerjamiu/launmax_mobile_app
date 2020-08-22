3f1fbcad53806b3dbe87663371f9ea0ee347c11b
Francisop <francisohis@gmail.com>
2020-08-22 14:34:51 +0100

    70% done

--- lib/UI/screens/order/order_detail.dart
+++ lib/UI/screens/order/order_detail.dart
@@ -11,124 +11,159 @@ class OrderDetail extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     var newDate = DateFormat("MMMM d, yyyy HH:mm a").format(order.dateTime);
-    return Scaffold(backgroundColor: Colors.white,
-
-          appBar: AppBar(
+    return Scaffold(
+      backgroundColor: Colors.white,
+      appBar: AppBar(
         elevation: 0.0,
         centerTitle: true,
         backgroundColor: Colors.white,
-        leading: IconButton(icon: Icon(Icons.arrow_back,color: Colors.black,), onPressed: (){Navigator.pop(context);}),
+        leading: IconButton(
+            icon: Icon(
+              Icons.arrow_back,
+              color: Colors.black,
+            ),
+            onPressed: () {
+              Navigator.pop(context);
+            }),
         title: Text(
           'Order Details',
-          style: TextStyle(fontWeight: FontWeight.w600, fontSize: 20,color: Colors.black),
+          style: TextStyle(
+              fontWeight: FontWeight.w600, fontSize: 20, color: Colors.black),
         ),
       ),
       body: Padding(
         padding: const EdgeInsets.all(18.0),
         child: Card(
-          elevation: 9.9,
+            elevation: 9.9,
             child: Column(
-          children: [
-            Container(
-                child: Row(
-              children: [],
-            )),
-            Divider(),
-            Container(
-              padding: EdgeInsets.all(20.1),
-              alignment: Alignment.centerLeft,
-              child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                  children: [
-                    Text(
-                      "Order No̲${order.orderNo}",
-                      style:
-                          TextStyle(fontSize: 25.0, color: AppColor.primaryColor),
-                    ),
-                    Row(
+              children: [
+                Container(
+                    padding:
+                        EdgeInsets.only(left: 5, right: 5, top: 10, bottom: 10),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                       children: [
-                        Text('item: ',
-                            style: TextStyle(
-                                fontSize: 17.0, color: Colors.grey[300])),
-                        Expanded(
-                          child: Text(
-                              '${order.items.reduce((v, e) => v + ',' + e)}',
-                              style: TextStyle(
-                                  fontSize: 17.0,
-                                  color: AppColor.primaryColor[600])),
+                        Icon(
+                          order.statusIcon,
+                          color: order.statusColor,
                         ),
+                        Text(order.status,
+                            style: TextStyle(color: order.statusColor)),
+                        SizedBox(
+                            width: MediaQuery.of(context).size.width * 0.06),
+                        Text("$newDate"),
                       ],
-                    ),
-                  ]),
-            ),
-            Divider(),
-            Container(
-              child: Row(
-                children: [
-                  Column(
+                    )),
+                Divider(),
+                Container(
+                  padding: EdgeInsets.all(20.1),
+                  alignment: Alignment.centerLeft,
+                  child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                      children: [
+                        Text(
+                          "Order No̲${order.orderNo}",
+                          style: TextStyle(
+                              fontSize: 25.0, color: AppColor.primaryColor),
+                        ),
+                        Row(
+                          children: [
+                            Text('item: ',
+                                style: TextStyle(
+                                    fontSize: 17.0, color: Colors.grey[300])),
+                            Expanded(
+                              child: Text(
+                                  '${order.items.reduce((v, e) => v + ',' + e)}',
+                                  style: TextStyle(
+                                      fontSize: 17.0,
+                                      color: AppColor.primaryColor[600])),
+                            ),
+                          ],
+                        ),
+                      ]),
+                ),
+                Divider(),
+                Container(
+                  padding: EdgeInsets.all(8),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceAround,
                     children: [
-                      Text(
-                        "VALUE OF ITEMS ",
-                        style: TextStyle(fontSize: 15, color: Colors.grey[400]),
+                      Column(
+                        children: [
+                          Text(
+                            "VALUE OF ITEMS ",
+                            style: TextStyle(
+                                fontSize: 15, color: Colors.grey[400]),
+                          ),
+                          Text(
+                            "NGN ${order.totalCost}",
+                            style: TextStyle(fontWeight: FontWeight.w600),
+                          ),
+                        ],
                       ),
-                      Text(
-                        "NGN ${order.totalCost}",
-                        style: TextStyle(fontWeight: FontWeight.w600),
+                      Column(
+                        children: [
+                          Text(
+                            "QUANTITY ",
+                            style: TextStyle(
+                                fontSize: 15, color: Colors.grey[400]),
+                          ),
+                          Text(
+                            "${order.quantity} pairs",
+                            style: TextStyle(
+                                fontWeight: FontWeight.w900, fontSize: 20),
+                          )
+                        ],
                       ),
                     ],
                   ),
-                  Column(
+                ),
+                Divider(),
+                Container(
+                  padding: EdgeInsets.all(20.1),
+                  alignment: Alignment.centerLeft,
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
                     children: [
                       Text(
-                        "QUANTITY ",
-                        style: TextStyle(fontSize: 15, color: Colors.grey[400]),
+                        'DELIVERY DETAILS\n',
+                        style: TextStyle(color: Colors.grey[500], fontSize: 20),
                       ),
-                      Text(
-                        "${order.quantity}",
-                        style:
-                            TextStyle(fontWeight: FontWeight.w900, fontSize: 20),
-                      )
+                      Text('Time: $newDate\n'),
+                      Text('Address: ${order.deliveryAddress}'),
                     ],
                   ),
-                ],
-              ),
-            ),
-            Divider(),
-            Container(
-              child: Column(
-                children: [
-                  Text('DELIVERY DETAILS'),
-                  Text('Time: $newDate'),
-                  Text('Address: ${order.deliveryAddress}'),
-                ],
-              ),
-            ),
-            Divider(),
-            Container(
-              child: Column(
-                children: [
-                  Text("DRY CLEANER'S DETAILS"),
-                  Text('Company Name: ${order.companyName}'),
-                  Text('Delivery Man: ${order.deliveryPersonel}'),
-                  Row(
+                ),
+                Divider(),
+                Container(
+                  padding: EdgeInsets.all(20.1),
+                  alignment: Alignment.centerLeft,
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
                     children: [
-                      Text('Phone Number: ${order.phoneNumber}'),
-                      FlatButton.icon(
-                        color: Colors.red,
-                        icon: Icon(Icons.phone), //`Icon` to display
-                        label: Text('Call'), //`Text` to display
-                        onPressed: () {
-                         
-                        },
+                      Text(
+                        "DRY CLEANER'S DETAILS\n",
+                        style: TextStyle(color: Colors.grey[500], fontSize: 20),
+                      ),
+                      Text('Company Name: ${order.companyName}\n'),
+                      Text('Delivery Man: ${order.deliveryPersonel}'),
+                      Row(
+                        children: [
+                          Text('Phone: ${order.phoneNumber}'),
+                          FlatButton.icon(
+                            // color: Colors.red,
+                            icon: Icon(Icons.phone), //`Icon` to display
+                            label: Text('Call'), //`Text` to display
+                            onPressed: () {},
+                          ),
+                        ],
                       ),
                     ],
                   ),
-                ],
-              ),
-            )
-          ],
-        )),
+                )
+              ],
+            )),
       ),
     );
   }
